1fe1bf145f1083209371b4c41ad656d4e93e18ae
muzammal-invozone <muzammil.ikram@invozone.com>
2023-05-05 12:19:01 +0500

    .

--- KFC-Pakistan-Clone/src/main.jsx
+++ KFC-Pakistan-Clone/src/main.jsx
@@ -1,17 +1,78 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { ChakraProvider } from '@chakra-ui/react'
-import { BrowserRouter } from 'react-router-dom'
-import customTheme from './utils/theme.jsx'
+import React, { useState } from "react";
+import ReactDOM from "react-dom/client"; 
+import {API_RESPONSE} from './utils/api';
+// Functional Component
+const Header = () => {
+    return (
+        <header style={{display: 'flex', justifyContent: 'space-between', alignItems: 'center', border: '1px solid black'}}>
+            <img src="https://img.freepik.com/premium-vector/online-food-order-logo-icon_61778-45.jpg" alt="logo" style={{width: '120px'}} />
+            <nav>
+                <ul style={{    display: 'flex', columnGap: '10px', marginRight: '20px' }}>
+                    <li style={{listStyleType: 'none', cursor: 'pointer'}}>Home</li>
+                    <li style={{listStyleType: 'none', cursor: 'pointer'}}>About Us</li>
+                    <li style={{listStyleType: 'none', cursor: 'pointer'}}>Contact Us</li>
+                    <li style={{listStyleType: 'none', cursor: 'pointer'}}>Cart [0]</li>
+                    <li style={{listStyleType: 'none', cursor: 'pointer'}}>Login / Logout</li>
+                </ul>
+            </nav>
+        </header>
+    )
+};
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <ChakraProvider theme={customTheme}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </ChakraProvider>
-  </React.StrictMode>,
-)
+const FoodContainer = ({    data}) => {
+    return (
+        <div style={{
+            width: '250px',
+            border: '1px solid',
+            padding: '15px'
+        }}>
+            <img src={'https://images.kfcpakistan.com/' + data.imageUrl} alt="food image" style={{    width: '250px'}} />
+            <h4>{data.name}</h4>
+            <p>{data.productDescription}</p>
+            <h4>{data.originalPrice}</h4>
+            <button>Add to Card</button>
+        </div>
+    )
+};
+
+const Body = () => {
+    const [value , setValue] = useState(10);
+    const [res , setRes] = useState(API_RESPONSE);
+    function removeProduct() {
+        setRes(res);
+        // setValue(20); 
+        // console.log(API_RESPONSE)
+    }
+    return (
+        <>
+            <button onClick={() => removeProduct()}>Click here {value}</button>
+            <div style={{display: 'flex', justifyContent: 'space-between'}}>
+                {API_RESPONSE.map((data) => {
+                    return (
+                        <FoodContainer data={data} key={data.id}/> 
+                    )
+                })}
+            </div>
+        
+        </>
+    )
+};
+
+const Footer = () => {
+    return (
+        <h1>Footer</h1>
+    )
+};
+
+const App = () => {
+    return (
+        <>
+            <Header />
+            <Body />
+            <Footer />
+        </>
+    )
+}
+
+const root = ReactDOM.createRoot(document.getElementById('react-root'));
+root.render(<App />);
---
+++ KFC-Pakistan-Clone/src/utils/api.js
@@ -0,0 +1,38 @@
+export const API_RESPONSE = [
+  {
+    "id": 36,
+    "name": "Krunch Burger",
+    "imageUrl": "image/1670410356108-image.jpg",
+    "productDescription": "Crunchy chicken fillet, spicy mayo, lettuce, sandwiched between a sesame seed bun",
+    "originalPrice": 250.0,
+    "productType": "PRODUCT",
+    "isAvailableForSale": true
+  },
+  {
+    "id": 33,
+    "name": "Hot Wings",
+    "imageUrl": "image/1670410323079-image.jpg",
+    "productDescription": "10 Pcs of our Signature Hot & Crispy Wings",
+    "originalPrice": 550.0,
+    "productType": "PRODUCT",
+    "isAvailableForSale": true
+  },
+  {
+    "id": 23,
+    "name": "Krunch With Fries N Drink",
+    "imageUrl": "image/1670409700963-image.jpg",
+    "productDescription": "Krunch + 1 regular Fries + 1 regular Drink",
+    "originalPrice": 490.0,
+    "productType": "PACKAGE",
+    "isAvailableForSale": true
+  },
+  {
+    "id": 51,
+    "name": "Xtreme Duo Box",
+    "imageUrl": "image/1670410761440-image.jpg",
+    "productDescription": "The irresistible combo of 2 signature Zingers + 2 pcs Hot & Crispy Chicken + 1 large fries + 2 drinks",
+    "originalPrice": 1390.0,
+    "productType": "PACKAGE",
+    "isAvailableForSale": true
+  }
+];
